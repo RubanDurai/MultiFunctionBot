39704e10209cb3d74ab73ee2155ebe29c336df0f
NesamaniX1 <77948100+NesamaniX1@users.noreply.github.com>
2022-12-12 19:44:39 +0530

    Update version.py
    Signed-off-by: NesamaniX1 <77948100+NesamaniX1@users.noreply.github.com>

--- bot/version.py
+++ bot/version.py
@@ -20,5 +20,5 @@ __python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 __bot_version__ = get_version()
 __pyro_version__ = __pyrog_version__
 __pyro_layer__ = layer
-__license__ = "[GNU Affero General Public License v3.0](https://github.com/missemily22/MultiFunctionBot/blob/main/LICENSE)"
-__gitrepo__ = "https://github.com/missemily22/MultiFunctionBot"
+__license__ = "GNU Affero General Public License v3.0"
+__gitrepo__ = "https://t.me/TNROCKERS2021"
